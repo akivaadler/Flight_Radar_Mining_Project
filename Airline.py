c683c0ecf726670a8bfcf091c775812a84236597
AkivaAdler <akivaadler@gmail.com>
2021-09-02 16:56:57 +0300

    Update the Airline.py file

--- Airline.py
+++ Airline.py
@@ -1,4 +1,5 @@
 class Airline:
+    """Builds class that contains information about individual airlines"""
     def __init__(self, name, code, num_aircraft, routes, fleet, reviews):
         self.name = name
         self.code = code
@@ -7,5 +8,18 @@ class Airline:
         self.fleet = fleet
         self.reviews = reviews
 
+    def _get_string(self):
+        airline_str = ''
+        airline_str += f'Airline Name: {self.name} \n'
+        airline_str += f'Airline Code: {self.code} \n'
+        airline_str += f'Airline Size of Fleet: {self.num_aircraft} \n'
+        airline_str += f'Airline Routes: {self.routes} \n'
+        airline_str += f'Airline Fleet: {self.fleet} \n'
+        airline_str += f'Airline Reviews: {self.reviews} \n'
+        return airline_str
+
     def __str__(self):
-        return f'Airline: {str(self.__dict__)}'
+        return self._get_string()
+
+    def __repr__(self):
+        return self._get_string()
